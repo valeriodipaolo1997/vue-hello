9611efb0d0c7ce81669107af07a968fb7d1f04f7
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-09-11 15:57:49 +0200

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+/* Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa inserendo 
+il percoso in una propietá dell'oggetto restituito dalla funzione data.*/
+
